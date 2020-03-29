dfbd5fc84e87735ca22254e741bb0712d636ab0b
Your Name <you@example.com>
2020-03-29 13:57:22 +0100

    set mouse mode as default

--- drivers/hid/hid-nvidia-blake.c
+++ drivers/hid/hid-nvidia-blake.c
@@ -484,7 +484,7 @@ static int nvidia_probe(struct hid_device *hdev, const struct hid_device_id *id)
 	loc->y = TOUCHPAD_DEFAULT_Y;
 	loc->action = 0;
 	loc->speed = DEFAULT_SPEED;
-	loc->mode = DISABLED_MODE;
+	loc->mode = MOUSE_MODE;
 	hid_set_drvdata(hdev, loc);
 
 	/* Parse the HID report now */
